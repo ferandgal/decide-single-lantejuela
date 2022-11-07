4bacaad527adfed91230c618154b03c554e19e9d
Juanmagc99 <juanma101099@gmail.com>
2022-11-07 21:06:44 +0100

    test : Test de formulario multiple censo #10 Testeo del formulario multiple para añadir usuarios al censo
    Co-authored-by: ZeldaXD <miguel_molina2@hotmail.es>
    
    closes #10

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -1,75 +1,59 @@
 import random
 from django.contrib.auth.models import User
 from django.test import TestCase
-from rest_framework.test import APIClient
+from django.test.testcases import TransactionTestCase
 
+from django.core.serializers import json
+from .forms import CensusForm
 from .models import Census
 from base import mods
 from base.tests import BaseTestCase
+from voting.models import Question, Voting
+from datetime import date
 
-
-class CensusTestCase(BaseTestCase):
-
+#Use TransactionTestCase instead of BaseTestCase
+class CensusTestCase(TransactionTestCase):
+    reset_sequences = True
     def setUp(self):
         super().setUp()
-        self.census = Census(voting_id=1, voter_id=1)
-        self.census.save()
+        question1 = Question.objects.create(desc='Question 1')
+        voting1 = Voting.objects.create(id=10, name='Voting', question = question1)
+        voting1.save()
 
+        self.census = Census(voting_id=10, voter_id=1)
+        self.census.save()
+    
     def tearDown(self):
         super().tearDown()
+        self.voting1 = None
+        self.admin = None
+        self.user1 = None
+        self.user2 = None
         self.census = None
+    
+    def test_add_voter_custom_post(self):
+        admin = User(username='admin2', password='qwerty')
+        admin.is_staff = True
+        admin.save()
 
-    def test_check_vote_permissions(self):
-        response = self.client.get('/census/{}/?voter_id={}'.format(1, 2), format='json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json(), 'Invalid voter')
-
-        response = self.client.get('/census/{}/?voter_id={}'.format(1, 1), format='json')
+        self.client.force_login(admin)
+        response = self.client.post('/census/addCustom/', 
+        data={'voting': ['10'], 'voter_ids': [admin.pk]})
+        census = Census.objects.all()
+        
+        print('----------------------------------------------------------------------\n')
+        print('Census: ', census)
+        print(response)
+        print('\n----------------------------------------------------------------------\n')
+        
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), 'Valid voter')
+        self.assertEqual(len(census), 1)
+    
+    def test_add_voter_custom_get(self):
 
-    def test_list_voting(self):
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
-        self.assertEqual(response.status_code, 401)
-
-        self.login(user='noadmin')
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
-        self.assertEqual(response.status_code, 403)
-
-        self.login()
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+        admin = User(username='admin2', password='qwerty')
+        admin.is_staff = True
+        admin.save()
+        self.client.force_login(admin)
+        response = self.client.get('/census/addCustom/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'voters': [1]})
-
-    def test_add_new_voters_conflict(self):
-        data = {'voting_id': 1, 'voters': [1]}
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 401)
-
-        self.login(user='noadmin')
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 403)
-
-        self.login()
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 409)
-
-    def test_add_new_voters(self):
-        data = {'voting_id': 2, 'voters': [1,2,3,4]}
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 401)
-
-        self.login(user='noadmin')
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 403)
-
-        self.login()
-        response = self.client.post('/census/', data, format='json')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)
-
-    def test_destroy_voter(self):
-        data = {'voters': [1]}
-        response = self.client.delete('/census/{}/'.format(1), data, format='json')
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(0, Census.objects.count())
