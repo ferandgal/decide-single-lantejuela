7f29f6e9ed13ad60070a2d52debedfa22daeebfc
pepelolo <josruiper4@alum.us.es>
2022-12-11 18:36:57 +0100

    feat: Pintado de gráficas y estudio de datos #28
    Implementadas dos gráficas para visualizar los resultados de las votaciones
    
    Closes #28

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -15,8 +15,51 @@ class VisualizerView(TemplateView):
 
         try:
             r = mods.get('voting', params={'id': vid})
+            optionsG = r[0].get('question').get('options')
+            optionsName = []
+
+            for dicc in optionsG:
+                optionsName.append(dicc.get('option'))
+
+            votesG = r[0].get('postproc')
+            numbVotes = []
+
+            for dicc in votesG:
+                numbVotes.append(dicc.get('postproc'))
+            print(context)
+
             context['voting'] = json.dumps(r[0])
+            context['optVotes'] = optionsName
+            context['numVotes'] = numbVotes
+            print(context)
         except:
             raise Http404
 
         return context
+
+    # def get_votes_graphic(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     vid = kwargs.get('voting_id', 0)
+
+    #     try:
+    #         r = mods.get('voting', params={'id': vid})
+    #         optionsG = r[0].get('question').get('options')
+    #         optionsName = []
+
+    #         for dicc in optionsG:
+    #             optionsName.append(dicc.get('option'))
+
+    #         votesG = r[0].get('postproc')
+    #         numbVotes = []
+
+    #         for dicc in votesG:
+    #             numbVotes.append(dicc.get('postproc'))
+    #         print(optionsName)
+    #         print(json.dumps(optionsName))
+
+    #         context['optVotes'] = json.dumps(optionsName)
+    #         context['numVotes'] = numbvotes
+    #     except:
+    #         raise Http404
+
+    #     return context
