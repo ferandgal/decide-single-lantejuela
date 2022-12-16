221eae2e755a9014c4a22a82bf88a98f8ec15973
fravarcas <xxraptor51esp@gmail.com>
2022-12-16 15:33:23 +0100

    fix : arreglar github actions
    Se ha modificado la base de datos a una existente

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
+        'NAME': 'decide19',
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': 'localhost',
